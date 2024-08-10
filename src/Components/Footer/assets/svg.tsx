ba0d98d0f8303b7d6a75b6eb019d9d1d2a931deb
mhdaslam <mhdaslamktd@gmail.com>
2024-08-10 21:38:18 +0530

    x modifies

--- src/Components/Footer/assets/svg.tsx
+++ src/Components/Footer/assets/svg.tsx
@@ -30,6 +30,7 @@ export const Instagram = () => {
     </svg>
   );
 };
+
 export const X = () => {
   return (
     <svg
@@ -46,6 +47,8 @@ export const X = () => {
     </svg>
   );
 };
+
+
 export const Youtube = () => {
   return (
     <svg
@@ -63,21 +66,17 @@ export const Youtube = () => {
   );
 };
 
-export const twitter = () => {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="22"
-      height="22"
-      viewBox="0 0 24 24"
-      fill="none"
-    >
-      <path
-        d="M3 3L21 21M3 21L21 3"
-        stroke="#EBD7FF"
-        stroke-width="2"
-        stroke-linecap="round"
-      />
-    </svg>
-  );
-};
+
+
+
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+ version="1.1"
+  width="22px" 
+  height="22px" 
+  viewBox="0 0 24 24" 
+  fill="none"
+
+<path 
+d="M14.095479,10.316482L22.286354,1h-1.940718l-7.115352,8.087682L7.551414,1H1l8.589488,12.231093L1,23h1.940717  l7.509372-8.542861L16.448587,23H23L14.095479,10.316482z M11.436522,13.338465l-0.871624-1.218704l-6.924311-9.68815h2.981339  l5.58978,7.82155l0.867949,1.218704l7.26506,10.166271h-2.981339L11.436522,13.338465z"/>
+fill="#EBD7FF"
+ </svg>
