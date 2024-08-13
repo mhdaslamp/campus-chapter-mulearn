7d3c3a361dc74d9b7ce82036932686bd2c9274a8
mhdaslam <mhdaslamktd@gmail.com>
2024-08-13 20:40:09 +0530

    added x logo

--- src/Components/Footer/assets/svg.tsx
+++ src/Components/Footer/assets/svg.tsx
@@ -35,8 +35,6 @@ export const X = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-      version="1.1"
       width="22px"
       height="22px"
       viewBox="0 0 24 24"
